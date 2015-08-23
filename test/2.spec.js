83bfcbe0a3623ac48440fcd9b2dd5f33bfc65cd3
james@jamespamplin.com <james@jamespamplin.com>
2015-08-23 11:13:24 +0100

    Ensure space-after-keywords

--- test/2.spec.js
+++ test/2.spec.js
@@ -6,6 +6,13 @@ describe( '2. Beautiful Syntax', function() {
 
   describe( 'A. Parens, Braces, Linebreaks', function() {
 
+    it( 'should fail when cramped syntax', function() {
+      var report = eslint.executeOnText('while(true) i++;');
+
+      expect(report.results[0].errorCount).to.equal(1);
+      expect(report.results[0].messages[0].ruleId).to.equal('space-after-keywords');
+    } );
+
     it( 'should show no errors or warnings on idomatic source', function() {
 
       var report = eslint.executeOnFiles(['./test/fixtures/2.A.1.1.js']);
