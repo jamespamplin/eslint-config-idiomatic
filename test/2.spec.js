ba91d4854ead4c7529f94f617a87fedac6bc2ffb
james@jamespamplin.com <james@jamespamplin.com>
2015-08-23 08:41:38 +0100

    Init mocha tests. Working tests for 2.A

--- test/2.spec.js
+++ test/2.spec.js
@@ -1,6 +1,32 @@
-var CLIEngine = require("eslint").CLIEngine;
+var CLIEngine = require('eslint').CLIEngine;
 var cli = new CLIEngine();
 
-var report = cli.executeOnFiles(["./test/fixtures/"]);
+var expect = require('chai').expect;
 
-console.log(JSON.stringify(report, null, '  '));
+
+describe( '2. Beautiful Syntax', function() {
+
+  describe( 'A. Parens, Braces, Linebreaks', function() {
+
+    it( 'should show no errors or warnings on idomatic source', function() {
+
+      var report = cli.executeOnFiles(['./test/fixtures/2.A.1.1.js']);
+
+      expect(report.results[0].errorCount).to.equal(0);
+      expect(report.results[0].warningCount).to.equal(0);
+
+    } );
+
+
+    it( 'should show errors on non-idomatic source', function() {
+
+      var report = cli.executeOnFiles(['./test/fixtures/2.A.1.1.bad.js']);
+
+      expect(report.results[0].errorCount).to.not.equal(0);
+      expect(report.results[0].warningCount).to.equal(0);
+
+    } );
+
+  } );
+
+} );
