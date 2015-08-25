b04c52cea01146e2d63dd82b35f8b7bb817ca440
james@jamespamplin.com <james@jamespamplin.com>
2015-08-25 06:47:20 +0100

    Rule: comma-style last

---
+++ test/appendix.spec.js
@@ -0,0 +1,10 @@
+describe( 'Appendix', function() {
+  describe( 'Comma first', function() {
+    it( 'should fail when using comma first style', function() {
+      var src = 'var first = 1\n' +
+      '  , second = 2;\n';
+
+      expect( src ).to.have.eslintErrors( [ 'comma-style' ] );
+    } );
+  } );
+} );
