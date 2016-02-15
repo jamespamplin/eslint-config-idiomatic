a9a59055200f031f5e84f6d8bf7065a93f89769f
james@jamespamplin.com <james@jamespamplin.com>
2016-02-15 21:43:22 +0000

    Fixed bad spelling of fixture source

--- test/fixtures.spec.js
+++ test/fixtures.spec.js
@@ -3,7 +3,7 @@ var eslint = require( './support/eslint-adapter' );
 describe( 'fixtures', function testFixtures() {
 
   it( 'should not have eslint errors on idomatic-sample.js', function testIdomaticSample() {
-    var report = eslint.executeOnFiles( [ './test/fixtures/idomatic-sample.js' ] ),
+    var report = eslint.executeOnFiles( [ './test/fixtures/idiomatic-sample.js' ] ),
       messages = eslint.formatReportResults( report.results );
 
     expect( report.results[ 0 ].errorCount, messages ).to.equal( 0 );
