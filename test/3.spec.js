69b1350ac42568032200792a3bd973f615b861a7
james@jamespamplin.com <james@jamespamplin.com>
2016-05-29 23:23:31 +0100

    Remove `no-eq-null` enforcement.
    - Fixes #7

---
+++ test/3.spec.js
@@ -0,0 +1,19 @@
+/*eslint no-unused-expressions:0, func-names:0 */
+
+describe( '3. Type Checking', function() {
+
+  describe( 'A. Actual Types', function() {
+
+    it( 'should not fail when comparing against null', function() {
+      var src = 'variable == null;\n';
+      expect( src ).to.not.have.eslintErrors;
+    } );
+
+    it( 'should not fail when comparing against null (strong type equal)', function() {
+      var src = 'variable === null;\n';
+      expect( src ).to.not.have.eslintErrors;
+    } );
+
+  } );
+
+} );
